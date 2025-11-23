b560e9464477fec5aea0ea1cd650d2fba050346f
AlokAg <alok.agrawal@blinkx.in>
2025-11-24 01:48:51 +0530

    axios file modified

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.NEXT_PUBLIC_API_URL || 'http://localhost:8090/api',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8090/api',
     headers: {
         'Content-Type': 'application/json',
         'Cache-Control': 'no-cache, no-store, must-revalidate',
